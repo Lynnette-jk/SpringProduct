0eaee911733d08ea35f0c9985d695484534011a4
Lynnette_jk <lynnettecheruto@gmail.com>
2023-06-16 16:44:18 +0300

    listening to context events

--- src/main/java/com/june/MainApp.java
+++ src/main/java/com/june/MainApp.java
@@ -2,13 +2,21 @@ package com.june;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.annotation.*;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext ctx = new AnnotationConfigApplicationContext(TextEditorConfig.class);
-      TextEditor te = ctx.getBean(TextEditor.class);
-      te.spellCheck();
+	   ConfigurableApplicationContext context = 
+		         new ClassPathXmlApplicationContext("Beans.xml");
+	// Let us raise a start event.
+	      context.start();
+		  
+	      Product obj = (Product) context.getBean("product");
+	      obj.getMessage();
+
+	      // Let us raise a stop event.
+	      context.stop();
    }
 }
