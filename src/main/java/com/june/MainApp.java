5ef86a5e7b6c72aa8375597b4206eb5c8b531bd9
Lynnette_jk <lynnettecheruto@gmail.com>
2023-06-16 15:35:03 +0300

    inner bean

--- src/main/java/com/june/MainApp.java
+++ src/main/java/com/june/MainApp.java
@@ -8,13 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
    public static void main(String[] args) {
       ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-      Product objA = (Product) context.getBean("product");
-      objA.getMessage1();
-      objA.getMessage2();
-     
-      ProductDrinks objB = (ProductDrinks) context.getBean("productDrinks");
-      objB.getMessage1();
-      objB.getMessage2();
-      objB.getMessage3();
+      TextEditor te = (TextEditor) context.getBean("textEditor");
+      te.spellCheck();
    }
 }
